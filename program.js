f31a274f8454cd58dd93bac9257dbda68cfaebcf
Marcin Bogdanski <marcin@bogdanski.info>
2018-03-30 12:00:21 +0200

    capital letter add

--- program.js
+++ program.js
@@ -11,7 +11,7 @@ process.stdin.on('readable', function () {
         break
       case 'info':
         console.log('Node version: ' + process.versions.node)
-        console.log('Lang version: ' + process.env.lang)
+        console.log('Lang version: ' + process.env.LANG)
         break
       default:
         process.stdout.write('Wrong instruction!\n')
